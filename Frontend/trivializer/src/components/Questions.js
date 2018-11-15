03b315072792b133ffb84823d45aa2e6849f592d
TangledTessellations <mcevoya@oregonstate.edu>
2018-11-15 13:55:41 -0800

    Add state to questions component

--- Frontend/trivializer/src/components/Questions.js
+++ Frontend/trivializer/src/components/Questions.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Questions extends Component {
-    render() {
-        return <div>Questions</div>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameId: this.props.gameID,
+      roundname: this.props.roundname,
+      numberOfQuestions: this.props.numberOfQuestions,
+      category: this.props.category,
+      difficulty: this.props.difficulty,
+      type: this.props.type,
+      questions: "",
+      baseURL: "https://opentdb.com/api.php?"
+    };
+  }
+
+  componentDidMount = () => {
+    //   Prepare arguments to questions API
+    let amount = `amount=${this.state.numberOfQuestions || 1}`;
+    let category = `category=${this.state.category || "none"}`;
+    let difficulty = `difficulty=${this.state.difficulty || "none"}`;
+    let type = `type=${this.state.type || "none"}`;
+
+    //   Call axios with input parameters
+  };
+  render() {
+    return <div>Questions</div>;
+  }
 }
 
 export default Questions;
