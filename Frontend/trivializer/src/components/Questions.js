3f085bb53202c6446240292d717bbb443684ea41
TangledTessellations <mcevoya@oregonstate.edu>
2018-11-15 16:31:19 -0800

    Crude display of questions

--- Frontend/trivializer/src/components/Questions.js
+++ Frontend/trivializer/src/components/Questions.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
 class Questions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: this.props.question.question,
+      correct: this.props.question.correct_answer,
+      incorrect: this.props.question.incorrect_answers,
+      all: []
+    };
+  }
+
+  componentDidMount = () => {
+    let incorrect = this.state.incorrect;
+    let correct = this.state.correct;
+    // Get a random number, this will be where we insert the correct answer into the incorrect answers
+    let index = Math.floor(Math.random() * (incorrect.length + 1));
+    // Splice into index, delete 0 elements, insert the correct answer
+    let all = incorrect;
+    all.splice(index, 0, correct);
+    console.log("all: ", all);
+    this.setState({ all: all });
+  };
+
   render() {
-    return <div>Questions</div>;
+    console.log("this.state.all: ", this.state.all);
+    return (
+      <div>
+        <div>{this.state.question}</div>
+        <div>
+          <ul>
+            {this.state.all.map(answer => {
+              return <li>{answer}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
--- Frontend/trivializer/src/components/Round.js
+++ Frontend/trivializer/src/components/Round.js
@@ -1,42 +1,81 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
+import Questions from "./Questions";
 import axios from "axios";
 
 class Round extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameId: this.props.gameID,
-      roundname: this.props.roundname,
-      numberOfQuestions: this.props.numberOfQuestions,
-      category: this.props.category,
-      difficulty: this.props.difficulty,
-      type: this.props.type,
-      questions: "",
+      gameId: this.props.gameID || 1,
+      roundname: this.props.roundname || "Round 1",
+      numberOfQuestions: this.props.numberOfQuestions || 5,
+      category: this.props.category || "",
+      difficulty: this.props.difficulty || "easy",
+      type: this.props.type || "multiple",
+      questions: [],
       baseURL: "https://opentdb.com/api.php?"
     };
   }
 
   componentDidMount = () => {
+    console.log("this.state: ", this.state);
     //   Prepare arguments to questions API
-    let amount = `amount=${this.state.numberOfQuestions || 1}`;
-    let category = `category=${this.state.category || "none"}`;
-    let difficulty = `difficulty=${this.state.difficulty || "none"}`;
-    let type = `type=${this.state.type || "none"}`;
+
+    // Check is each is undefined, if it is, don't include it in the URL
+    let amount = `&amount=${this.state.numberOfQuestions || 1}`;
+    let category = `${
+      this.state.category ? `&category=${this.state.category}` : ""
+    }`;
+    let difficulty = `${
+      this.state.difficulty ? `&difficulty=${this.state.difficulty}` : ""
+    }`;
+    let type = `${this.state.category ? `&type=${this.state.type}` : ""}`;
 
     let concatenatedURL = `${
       this.state.baseURL
-    }&${amount}&${category}&${difficulty}&${type}`;
+    }${amount}${category}${difficulty}${type}`;
+
+    console.log("concatenatedURL: ", concatenatedURL);
     //   Call axios with input parameters
 
-    axios.get(concatenatedURL).then(res => {
-      console.log(res);
+    axios.get(concatenatedURL).then(response => {
+      this.setState({ questions: response.data.results });
     });
   };
 
   render() {
+    console.log("this.state.questions: ", this.state.questions);
     return (
-      <div>
-        <div>Round</div>
+      <div className="game-page">
+        <div className="top-content">
+          <div className="top-leftside">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  Games
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <Link className="top-rightside" to="/">
+            Sign Out
+          </Link>
+        </div>
+
+        <div className="main-content">
+          <Navbar />
+          <div>Questions</div>
+          <div>
+            {this.state.questions.map((question, index) => {
+              return <Questions key={index} question={question} />;
+            })}
+          </div>
+        </div>
       </div>
     );
   }
