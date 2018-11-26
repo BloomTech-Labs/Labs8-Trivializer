eadb034dc9305266fe1cc4951ec0eaef91bf744d
Nicky Chen <nicky.chen0@gmail.com>
2018-11-26 14:40:25 -0800

    minor routing change

--- Frontend/trivializer/src/components/Setting.js
+++ Frontend/trivializer/src/components/Setting.js
@@ -2,33 +2,39 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 
-const Setting = () => {
-  return (
-    <div className="setting-page">
-      <div className="top-content">
-        <div className="top-leftside">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li class="breadcrumb-item active" aria-current="page">
-                Setting
-              </li>
-            </ol>
-          </nav>
+class Setting extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="setting-page">
+        <div className="top-content">
+          <div className="top-leftside">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  Setting
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <Link className="top-rightside" to="/">
+            Sign Out
+          </Link>
         </div>
-        <Link className="top-rightside" to="/">
-          Sign Out
-        </Link>
-      </div>
 
-      <div className="main-content">
-        <Navbar />
-        <h1 className="main-middle">Setting Page</h1>
+        <div className="main-content">
+          <Navbar />
+          <h1 className="main-middle">Setting Page</h1>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Setting;
