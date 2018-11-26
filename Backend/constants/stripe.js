c8ea958cfc07fb85b8e2772db48631ee44778647
sgoal2 <sgoal27@gmail.com>
2018-11-26 17:43:14 -0500

    Added comment for stripe,.

--- Backend/constants/stripe.js
+++ Backend/constants/stripe.js
@@ -2,7 +2,7 @@ const configureStripe = require("stripe");
 
 const STRIPE_SECRET_KEY =
   process.env.NODE_ENV === "production"
-    ? "sk_test_zxWylPmNYWnbiWQAgqvtz3Vh"
+    ? "sk_test_zxWylPmNYWnbiWQAgqvtz3Vh" //process.env.MY_SECRET_KEY doesnt work like this
     : "sk_test_zxWylPmNYWnbiWQAgqvtz3Vh";
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
