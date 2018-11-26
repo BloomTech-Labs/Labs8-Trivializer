5843d7c53aed4610d7915dccf3ca8c7c31b352c8
Nicky Chen <nicky.chen0@gmail.com>
2018-11-26 15:33:29 -0800

    adding setting

--- Frontend/trivializer/src/components/Setting.js
+++ Frontend/trivializer/src/components/Setting.js
@@ -30,7 +30,25 @@ class Setting extends React.Component {
 
         <div className="main-content">
           <Navbar />
-          <h1 className="main-middle">Setting Page</h1>
+          <div className="main-middle">
+            <h1 className="main-middle">Setting Page</h1>
+            <div className="name">
+              <p>Name: </p>
+              <input placeholder="Name" value="Mary's Awesome Trivia" />
+            </div>
+            <div className="email">
+              <p>Email: </p>
+              <input placeholder="Email" value="user@gmail.com" />
+            </div>
+            <div className="oldpassword">
+              <p>Old Password: </p>
+              <input placeholder="password" value="********" />
+            </div>
+            <div className="newpassword">
+              <p>New Password: </p>
+              <input placeholder="password" value="**********" />
+            </div>
+          </div>
         </div>
       </div>
     );
