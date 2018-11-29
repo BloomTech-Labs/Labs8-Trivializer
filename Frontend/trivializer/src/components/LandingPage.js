36768dec839f8b053cae3ca811211644a2e94cbb
sgoal2 <sgoal27@gmail.com>
2018-11-29 16:22:10 -0500

    updated email regex,.

--- Frontend/trivializer/src/components/LandingPage.js
+++ Frontend/trivializer/src/components/LandingPage.js
@@ -7,7 +7,7 @@ import "./LandingPage.css";
 import { auth, provider } from "./OAuth/firebase";
 
 const username_regex = /^[a-zA-Z0-9]{4,}$/;
-const email_regex = /^([a-z\d\.-]+)@([a-z\d-]{2,8})\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+const email_regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 const password_regex = /(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}/;
 
 function validate(field, regex) {
