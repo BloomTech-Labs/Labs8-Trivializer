ae7020c35227a624b4ca1d7b8f715fa0691afab4
Nicky Chen <nicky.chen0@gmail.com>
2018-12-06 16:28:05 -0800

    styled faqs page

--- Frontend/trivializer/src/components/Faqs.js
+++ Frontend/trivializer/src/components/Faqs.js
@@ -34,49 +34,49 @@ const Faqs = () => {
 
       <div className="main-content">
         <Navbar />
-        <div>
-          <h1>FAQ's</h1>
-          <div>
+        <div className="content-container">
+          <h1>Frequently Asked Questions</h1>
+          <div className="question-container">
             <p className="questions">Why should I sign up for an account? </p>
-            <p>
+            <p className="answers">
               Signing up for an account lets you keep the games on your account. If you get
               questions that you really like, and you want to save them, signing up for an account
               lets you do that.
             </p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">What do I get with a Paid membership? </p>
-            <p>
+            <p className="answers">
               With a free account, you can only create at most 3 games, and have each game be at
               most 5 questions. A paid membership lets you create as many games as you want with as
               many questions as you want.
             </p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">How long does my paid membership last?</p>
-            <p>Forever! This is a buy one time membership. </p>
+            <p className="answers">Forever! This is a buy one time membership. </p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">How do I add games?</p>
-            <p>
+            <p className="answers">
               You can add games by clicking on the "Games" Tab and hitting +, and type in a game
               name to get started. The calendar is to give you a sense of when you want to play.{" "}
             </p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">Why do my games go away? </p>
-            <p>If you are not logged in, the games don't save to an account.</p>
+            <p className="answers">If you are not logged in, the games don't save to an account.</p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">Can I change the ordering of the questions?</p>
-            <p>
+            <p className="answers">
               If you would like the ordering of questions to be different, you can click the
               question anywhere and drag it down to wherever you want it to be.
             </p>
           </div>
-          <div>
+          <div className="question-container">
             <p className="questions">How do I change just 1 question if it's one I don't like?</p>
-            <p>
+            <p className="answers">
               You can hit Replace question to take way the question you don't like. You can hit Undo
               as well if you decide you want it back.
             </p>
