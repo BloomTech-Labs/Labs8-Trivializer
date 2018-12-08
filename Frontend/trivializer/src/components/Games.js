51751146816b2520c7d61554f9910edcb552458a
TangledTessellations <mcevoya@oregonstate.edu>
2018-12-08 15:59:51 -0800

    Fixed POST to get games

--- Frontend/trivializer/src/components/Games.js
+++ Frontend/trivializer/src/components/Games.js
@@ -5,35 +5,41 @@ import React, { Component } from "react";
  * - renders props for each game
  */
 class Games extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        const created = new Date(this.props.game.dateCreated);
-        const played = new Date(this.props.game.datePlayed);
-
-        return (
-            <div className="game">
-                <div>{this.props.game.gamename}</div>
-                <div className="game-description">{this.props.game.description}</div>
-                {/* <div>Add game image</div> */}
-                <div>
-                    Created:{" "}
-                    {`${created.getMonth() +
-                        1}-${created.getDate()}-${created.getFullYear()}`}
-                </div>
-                <div>
-                    Played:{" "}
-                    {this.props.game.datePlayed === 0
-                        ? null
-                        : `${played.getMonth() + 1}-${played.getDate() +
-                        1}-${played.getFullYear()}`}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    console.log(
+      "this.props.game.dateCreated: ",
+      typeof this.props.game.dateCreated
+    );
+    console.log("this.props.game.datePlayed: ", this.props.game.datePlayed);
+    const created = new Date(parseInt(this.props.game.dateCreated));
+    const played = new Date(parseInt(this.props.game.datePlayed));
+    console.log("created: ", created);
+    console.log("played: ", played);
+    return (
+      <div className="game">
+        <div>{this.props.game.gamename}</div>
+        <div className="game-description">{this.props.game.description}</div>
+        {/* <div>Add game image</div> */}
+        <div>
+          Created:{" "}
+          {`${created.getMonth() +
+            1}-${created.getDate()}-${created.getFullYear()}`}
+        </div>
+        <div>
+          Played:{" "}
+          {this.props.game.datePlayed === 0
+            ? null
+            : `${played.getMonth() + 1}-${played.getDate() +
+                1}-${played.getFullYear()}`}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Games;
